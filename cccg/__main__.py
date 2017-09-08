6b4d56df5de2214ba35693d88e631545ea9fe1fa
Jim Edwards <jedwards@ucar.edu>
2017-09-08 16:15:12 -0600

    developing

--- cccg/__main__.py
+++ cccg/__main__.py
@@ -8,7 +8,9 @@ from gui.mainwindow_ui import Ui_MainWindow
 # CIME modules
 from Tools.standard_script_setup import *
 from CIME.XML.machines import Machines
-
+from CIME.XML.files import Files
+from CIME.XML.compsets import Compsets
+from CIME.XML.grids import Grids
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -16,15 +18,30 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__()
         self.setupUi(self)
 
+
     def MachineSelect(self, text):
+        """ Handle selection of Machine """
         machine = self.MachineList.Machobj.set_machine(text)
         compilers = self.MachineList.Machobj.get_value("COMPILERS").split(',')
+        self.CompilerList.setEnabled(True)
         self.CompilerList.clear()
         self.CompilerList.addItems(compilers)
         mpilibs = self.MachineList.Machobj.get_value("MPILIBS").split(',')
         mpilibs.append("mpi_serial")
+        self.MPILIBList.setEnabled(True)
         self.MPILIBList.clear()
         self.MPILIBList.addItems(mpilibs)
+
+    def CompsetSelect(self, text):
+        files = Files()
+        components = files.get_components("COMPSETS_SPEC_FILE")
+        for comp in components:
+            infile = files.get_value("COMPSETS_SPEC_FILE", {"component":comp})
+            compsetobj = Compsets(infile=infile, files=files)
+            longname, _, _ = compsetobj.get_compset_match(text)
+            if longname is not None:
+                self.CompsetLongName.setEnabled(True)
+                self.CompsetLongName.setText(longname)
         
         
 def main():
@@ -39,30 +56,41 @@ def main():
 
     app = QApplication(sys.argv)
     main_window = MainWindow()
+    list_models(main_window.ModelList)
     list_machines(main_window.MachineList)
-    list_compilers(main_window.MachineList, main_window.CompilerList)
+    list_compsets(main_window.CompsetList)
+    list_grids(main_window.ResList)
     main_window.MachineList.activated[str].connect(main_window.MachineSelect)
+    main_window.CompsetList.activated[str].connect(main_window.CompsetSelect)
+    
     main_window.show()
     sys.exit(app.exec_())
 
+def list_models(ModelList):
+    ModelList.addItems(("CESM","ACME"))
     
-
-
 def list_machines(MachineList):
     MachineList.Machobj = Machines()
     mach_list = MachineList.Machobj.list_available_machines()
     MachineList.addItems(mach_list)
 
-def list_compilers(MachineList, CompilerList):
-    mach = MachineList.currentText()
-    
+def list_compsets(CompsetList):
+    files = Files()
+    components = files.get_components("COMPSETS_SPEC_FILE")
+    for comp in components:
+        infile = files.get_value("COMPSETS_SPEC_FILE", {"component":comp})
+        compsetobj = Compsets(infile=infile, files=files)
+        _, compsets = compsetobj.return_all_values()
+        CompsetList.addItems(sorted(compsets.keys()))
+        CompsetList.insertSeparator(999)
 
-    
-def list_mpilibs(MachineList, MPILIBList):
-    mach = MachineList.currentText()
-    print "{}".format(mach)
 
-    
-
+def list_grids(ResList):
+    ResList.gridsobj = Grids()
+    _, _, all_grids = ResList.gridsobj.return_all_values()
+    for grid in all_grids:
+        print "gridname is {}".format(grid.keys())
+    #    ResList.addItems(all_grids.keys())
+        
 if __name__ == "__main__":
     main()
