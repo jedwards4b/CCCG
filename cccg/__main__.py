c7a5c4c8be1dd651f187e6d6080d6acb854ea56d
Jim Edwards <jedwards@ucar.edu>
2017-09-12 15:59:32 -0600

    add tables

--- cccg/__main__.py
+++ cccg/__main__.py
@@ -2,11 +2,16 @@
 # Standard Python modules
 import sys
 # PyQt Modules
-from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox, QFileDialog,\
+    QTableWidgetItem
+from PyQt5 import QtCore
 # CCCG modules
 from gui.mainwindow_ui import Ui_MainWindow
+from gui.casewindow_ui import Ui_CaseWindow
+
 # CIME modules
 from Tools.standard_script_setup import *
+from CIME.case import Case
 from CIME.XML.machines import Machines
 from CIME.XML.files import Files
 from CIME.XML.compsets import Compsets
@@ -17,8 +22,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
-
-
+        self._casenames = []
+        self.CaseWindow = {}
+        
     def MachineSelect(self, text):
         """ Handle selection of Machine """
         machine = self.MachineList.Machobj.set_machine(text)
@@ -47,7 +53,70 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 for grid in valid_grids:
                     self.ResList.addItem(grid[0])
 
+    def OpenCase(self):
+        filedialog = QFileDialog()
+        filedialog.setFileMode(QFileDialog.Directory)
+        if filedialog.exec_():
+            casename = filedialog.selectedFiles()[0]
+            if casename in self._casenames:
+                # Should be an error - case is already open
+                pass
+            else:
+                self._casenames.append(casename)
+                self.CaseWindow[casename] = CaseWindow(casename)
+                self.CaseWindow[casename].show()
+
                 
+class CaseWindow(QMainWindow, Ui_CaseWindow):
+    def __init__(self, casename):
+        super(CaseWindow, self).__init__()
+        self.setupUi(self)
+        with Case(casename) as case:
+            self.populate_case_tab(case)
+            self.populate_setup_tab(case)
+            self.populate_build_tab(case)
+            self.populate_run_tab(case)
+
+    def tablecellactivated(self):
+        print "Which table?"
+            
+    def populate_case_tab(self, case):
+        env_case = case.get_env('case')
+        self.populate_tab(env_case, self.CaseTabtableWidget)
+
+    def populate_setup_tab(self, case):
+        env_mach_pes = case.get_env('mach_pes')
+        self.populate_tab(env_mach_pes, self.SetuptableWidget)
+#        self.SetuptableWidget.cellActivated.connect(self.tablecellactivated)
+        
+    def populate_build_tab(self, case):
+        env_build = case.get_env('build')
+        self.populate_tab(env_build, self.BuildtableWidget)
+    
+    def populate_run_tab(self, case):
+        env_run = case.get_env('run')
+        self.populate_tab(env_run, self.RuntableWidget)
+    
+        
+        
+    def populate_tab(self, entry_id_obj, tableWidget):
+        entry_nodes = entry_id_obj.get_nodes("entry")
+        tableWidget.setHorizontalHeaderLabels(['Variable', 'Value'])
+        tableWidget.setColumnCount(2)
+        tableWidget.setRowCount(len(entry_nodes))
+        row = 0
+        for node in entry_nodes:
+            vid = node.get("id")
+            vid_entry = QTableWidgetItem(vid)
+            vid_entry.setFlags(QtCore.Qt.NoItemFlags)
+            value_entry = QTableWidgetItem(str(entry_id_obj.get_value(vid)))
+            value_entry.setFlags(QtCore.Qt.ItemIsSelectable)
+            tableWidget.setItem(row, 0, vid_entry)
+            tableWidget.setItem(row, 1, value_entry)
+            row += 1
+
+
+            
         
 def main():
     args = sys.argv
@@ -67,7 +136,7 @@ def main():
     list_grids(main_window.ResList)
     main_window.MachineList.activated[str].connect(main_window.MachineSelect)
     main_window.CompsetList.activated[str].connect(main_window.CompsetSelect)
-    
+    main_window.actionOpen.triggered.connect(main_window.OpenCase)
     main_window.show()
     sys.exit(app.exec_())
 
