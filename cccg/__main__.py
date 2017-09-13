c0b6a4f9dedb9ce28123061faa24ad1e54f060b2
Jim Edwards <jedwards@ucar.edu>
2017-09-13 16:34:29 -0600

    todays progress

--- cccg/__main__.py
+++ cccg/__main__.py
@@ -5,6 +5,10 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox, QFileDialog,\
     QTableWidgetItem
 from PyQt5 import QtCore
+# PyQtGraphics Modules
+from pyqtgraph.parametertree import ParameterTree, Parameter
+import pyqtgraph.parametertree.parameterTypes 
+
 # CCCG modules
 from gui.mainwindow_ui import Ui_MainWindow
 from gui.casewindow_ui import Ui_CaseWindow
@@ -53,24 +57,30 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 for grid in valid_grids:
                     self.ResList.addItem(grid[0])
 
-    def OpenCase(self):
+    def FindCaseDir(self):
         filedialog = QFileDialog()
         filedialog.setFileMode(QFileDialog.Directory)
         if filedialog.exec_():
             casename = filedialog.selectedFiles()[0]
-            if casename in self._casenames:
-                # Should be an error - case is already open
-                pass
-            else:
-                self._casenames.append(casename)
-                self.CaseWindow[casename] = CaseWindow(casename)
-                self.CaseWindow[casename].show()
+            self.OpenCase(casename)
 
+    def OpenCase(self, casename):
+        if casename in self._casenames:
+            # casename is already open - change focus?
+            pass
+        else:
+            self._casenames.append(casename)
+            self.CaseWindow[casename] = CaseWindow(casename)
+            self.CaseWindow[casename].show()
+        
+
+                
                 
 class CaseWindow(QMainWindow, Ui_CaseWindow):
     def __init__(self, casename):
         super(CaseWindow, self).__init__()
         self.setupUi(self)
+        self.setMouseTracking(1)
         with Case(casename) as case:
             self.populate_case_tab(case)
             self.populate_setup_tab(case)
@@ -80,49 +90,67 @@ class CaseWindow(QMainWindow, Ui_CaseWindow):
     def tablecellactivated(self):
         print "Which table?"
             
+
     def populate_case_tab(self, case):
-        env_case = case.get_env('case')
-        self.populate_tab(env_case, self.CaseTabtableWidget)
+        entry_id_obj = case.get_env('case')
+        self.populate_tab(case, entry_id_obj, self.CaseParameterTree, writable=False)
+
+    def populate_run_tab(self, case):
+        entry_id_obj = case.get_env('run')
+        self.populate_tab(case, entry_id_obj, self.RunParameterTree)
 
     def populate_setup_tab(self, case):
-        env_mach_pes = case.get_env('mach_pes')
-        self.populate_tab(env_mach_pes, self.SetuptableWidget)
-#        self.SetuptableWidget.cellActivated.connect(self.tablecellactivated)
-        
+        entry_id_obj = case.get_env('mach_pes')
+        self.populate_tab(case, entry_id_obj, self.SetupParameterTree)
+
     def populate_build_tab(self, case):
-        env_build = case.get_env('build')
-        self.populate_tab(env_build, self.BuildtableWidget)
-    
-    def populate_run_tab(self, case):
-        env_run = case.get_env('run')
-        self.populate_tab(env_run, self.RuntableWidget)
-    
-        
-        
-    def populate_tab(self, entry_id_obj, tableWidget):
+        entry_id_obj = case.get_env('build')
+        self.populate_tab(case, entry_id_obj, self.BuildParameterTree)
+
+    def populate_tab(self, case, entry_id_obj, parametertree, writable=True):
         entry_nodes = entry_id_obj.get_nodes("entry")
-        tableWidget.setHorizontalHeaderLabels(['Variable', 'Value'])
-        tableWidget.setColumnCount(2)
-        tableWidget.setRowCount(len(entry_nodes))
-        row = 0
         for node in entry_nodes:
             vid = node.get("id")
-            vid_entry = QTableWidgetItem(vid)
-            vid_entry.setFlags(QtCore.Qt.NoItemFlags)
-            value_entry = QTableWidgetItem(str(entry_id_obj.get_value(vid)))
-            value_entry.setFlags(QtCore.Qt.ItemIsSelectable)
-            tableWidget.setItem(row, 0, vid_entry)
-            tableWidget.setItem(row, 1, value_entry)
-            row += 1
+            type = entry_id_obj.get_type_info(vid)
+            if type == "integer":
+                type = "int"
+            elif type == "logical":
+                type = "bool"
+            elif type == "char":
+                type = "str"
+            elif type == "real":
+                type = "float"
+            if hasattr(entry_id_obj, "_component_value_list") and vid in entry_id_obj._component_value_list:
+                top_param = Parameter.create(name=vid, type='group')
+                for comp in entry_id_obj._components:
+                    name = vid + "_{}".format(comp)
+                    value = case.get_value(name)
+                    if value is not None:
+                        param = Parameter.create(name=name, type=type, value=value)
+                        param.setWritable(writable)
+                        top_param.addChild(param)
+                parametertree.addParameters(top_param)
+            else:
+                value = case.get_value(vid)
+                valid_values = entry_id_obj.get_valid_values(vid)
+                tip = entry_id_obj.get_description(node)
+                if valid_values is not None:
+                    if type == "str":
+                        type = 'list'
+                param = Parameter.create(name=vid, type=type, value=value, values=valid_values)
+                param.widget.setToolTip(tip)
+                param.setWritable(writable)
+                parametertree.addParameters(param)
 
 
-            
-        
 def main():
     args = sys.argv
     parser = argparse.ArgumentParser(usage="Fill this in",
                                      description="an experimental gui tool",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("-caseroot" , "--caseroot", default=None,
+                        help="Case directory to reference")
 
     CIME.utils.setup_standard_logging_options(parser)
     args = CIME.utils.parse_args_and_handle_standard_logging_options(args, parser)
@@ -136,7 +164,10 @@ def main():
     list_grids(main_window.ResList)
     main_window.MachineList.activated[str].connect(main_window.MachineSelect)
     main_window.CompsetList.activated[str].connect(main_window.CompsetSelect)
-    main_window.actionOpen.triggered.connect(main_window.OpenCase)
+    main_window.actionOpen.triggered.connect(main_window.FindCaseDir)
+    if args.caseroot:
+        main_window.OpenCase(args.caseroot)
+    
     main_window.show()
     sys.exit(app.exec_())
 
